1fe0348645bc7c809099ff0f36d7015edcae2bde
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-03-25 10:24:25 +0200

    cleaned up code making use of eventEmitter v0.2.0

--- app.js
+++ app.js
@@ -2,31 +2,29 @@ const fs = require("fs/promises");
 
 (async () => {
   const commandFileHandler = await fs.open("./command.txt", "r");
+
+  commandFileHandler.on("change", async () => {
+    //*get the size of the file
+    const size = (await commandFileHandler.stat()).size;
+    //* allocate a buffer to read the content of the file length
+    const buf = Buffer.alloc(size);
+    //*offset is the position in the buffer to start writing.
+    const offset = 0;
+    //*lenght is the number of bytes to read
+    const length = buf.byteLength;
+    //*position is the position in the file to start reading from
+    const position = 0;
+
+    //*We want to read the whole length of new content of the file after it has changed
+    await commandFileHandler.read(buf, offset, length, position);
+    console.log(buf.toString("utf-8"));
+  });
+
   const watcher = fs.watch("./command.txt");
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      console.log("File changed, reading new content...");
-
-      //*get the size of the file
-      const size = (await commandFileHandler.stat()).size;
-      //* allocate a buffer to read the content of the file length
-      const buf = Buffer.alloc(size);
-      //*offset is the position in the buffer to start writing.
-      const offset = 0;
-      //*lenght is the number of bytes to read
-      const length = buf.byteLength;
-      //*position is the position in the file to start reading from
-      const position = 0;
-
-      //*We want to read the new content of the file after it has changed
-      const content = await commandFileHandler.read(
-        buf,
-        offset,
-        length,
-        position,
-      );
-      console.log("New content:", content);
+      commandFileHandler.emit("change");
     }
   }
 })();
